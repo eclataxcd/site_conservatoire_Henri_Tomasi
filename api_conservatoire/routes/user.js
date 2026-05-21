ef2d4aad809c95e5ba465f1d32208df1bd0fa86a
eclataxcd <clara.drieux@gmail.com>
2026-05-21 16:45:31 +0200

    save

--- api_conservatoire/routes/user.js
+++ api_conservatoire/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
-const { validateAddUser } = require('../middlewares/authValidator');
+const { validateAddUser } = require('../middlewares/validation');
 
 // Route : GET /users/{email} 
 router.get('/users/:email', userController.getUser);
